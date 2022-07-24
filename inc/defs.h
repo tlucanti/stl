07cc8be3800d6b8ba987fa9357a7a58e1aaba408
tlucanti <prototype3628800@yandex.ru>
2022-07-24 21:43:48 +0300

    add defs.h for c files

---
+++ inc/defs.h
@@ -0,0 +1,25 @@
+
+#ifndef DEFS_H
+# define DEFS_H
+
+# ifndef WUR
+#  define WUR __attribute__((warn_unused_result))
+# endif /* WUR */
+
+# ifndef NOEXCEPT
+#  define NOEXCEPT __attribute__((__nothrow__))
+# endif /* NOEXCEPT */
+
+# ifndef LIKELY
+#  define LIKELY(__expr) __builtin_expect((__expr), 1)
+# endif /* LIKELY */
+
+# ifndef UNLIKELY
+#  define UNLIKELY(__expr) __builtin_expect((__expr), 0)
+# endif /* UNLIKELY */
+
+# ifndef __restrict
+#  define __restrict restrict
+# endif
+
+#endif /* DEFS_H */
